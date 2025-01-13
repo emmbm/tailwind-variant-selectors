da9cc8bba81bac56a79304b52726a6f2de695585
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-01-13 15:50:39 -0500

    Update match patterns

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,9 @@
-export const CSS_FILE_PATTERN = /.*\.(css|scss)$/;
+export const CSS_FILE_PATTERN = /.*\.(css|scss|sass|pcss|postcss)$/;
 
-export const CSS_ENTRY_PATTERN = /^((?!\?svelte&type=style).)+\.(css|scss)$/;
+export const CSS_ENTRY_PATTERN = /^((?!\?svelte&type=style).)+\.(css|scss|sass|pcss|postcss)$/;
 
 export const CSS_IMPORT_PATTERN = /@import\s+('|")(?<file>[^("|')]*)('|")/;
 
-export const VARIANT_ATRULE_PATTERN = /@variant\s+(?<name>.*[^\s])\s+\((?<selectors>.*[^;|\s])\)/;
+export const VARIANT_ATRULE_PATTERN = /@variant\s+(?<name>[^\s]*)\s+\((?<selectors>.*[^;])\)/;
 
 export const VARIANT_SELECTOR_PATTERN = /:variant\(([^)\n\r]*)\)/;
